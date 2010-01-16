697087b5135fa7e98ac7861dd92353a755fa8a86
Megax <megax@yeahunter.hu>
2010-01-16 14:14:40 +0000

    Added licence

--- src/ascnhalf-shared/Database/DBCStores.cpp
+++ src/ascnhalf-shared/Database/DBCStores.cpp
@@ -1,4 +1,22 @@
-/* AscNHalf */
+/*
+  * AscNHalf MMORPG Server
+  * Copyright (C) 2005-2007 Ascent Team <http://www.ascentemu.com/>
+  * Copyright (C) 2010 AscNHalf Team <http://ascnhalf.scymex.info/>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+  *
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  */
 
 #include "DBCStores.h"
 #include "DataStore.h"
