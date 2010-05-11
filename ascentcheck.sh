dabf4fa0913a5978ef309fe4a51ea63d2b8c5ffc
Twl <twl@root.hu>
2010-05-11 20:33:38 +0200

    * Create new branch: experimental * Used for: testing unstable new stuff, (very) compiler dependent things and features of the upcoming standard. * Add initial checker.

---
+++ ascentcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Ascent C++ standard checker."
+echo "Written by Twl 2010/05/11"
+echo "Checks for supported stuff from the new standard."
+echo ""
+echo "Press return to begin..."
+read
+
